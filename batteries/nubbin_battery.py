941c5ce8d9bac0ac2677d373c5ff4798a6001402
Nicholas Nasour <nicknasour@gmail.com>
2023-09-30 16:13:10 -0700

    Refactored batteries classes

--- batteries/nubbin_battery.py
+++ batteries/nubbin_battery.py
@@ -1,8 +1,15 @@
 from battery import Battery
+from datetime import datetime
 
 class NubbinBattery(Battery):
     def __init__(self, last_service_date, current_date):
-        super().__init__(last_service_date)
+        self.last_service_date = last_service_date
+        self.current_date = current_date
     
     def needs_service(self):
-        return self.last_service_date - self.current_date > 4
+        last_service_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
+        battery_needs_service = last_service_date <= self.current_date
+        if battery_needs_service:
+            return True
+        else:
+            return False
--- batteries/spindler_battery.py
+++ batteries/spindler_battery.py
@@ -1,8 +1,15 @@
 from battery import Battery
+from datetime import datetime
 
 class SpindlerBattery(Battery):
     def __init__(self, last_service_date, current_date):
-        super().__init__(last_service_date)
+        self.last_service_date = last_service_date
+        self.current_date = current_date
     
     def needs_service(self):
-        return self.last_service_date - self.current_date > 2
+        last_service_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        battery_needs_service = last_service_date <= self.current_date
+        if battery_needs_service:
+            return True
+        else:
+            return False
