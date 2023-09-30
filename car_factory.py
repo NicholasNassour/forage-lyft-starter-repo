95aa566f0c8fa65ed004cfaa08e0ee212bde3392
Nicholas Nasour <nicknasour@gmail.com>
2023-09-30 16:32:37 -0700

    updated car and car factory

--- car_factory.py
+++ car_factory.py
@@ -1,5 +1,40 @@
 from car import Car
-from batteries import NubbinBattery
-from batteries import SpindlerBattery
+from batteries.nubbin_battery import NubbinBattery
+from batteries.spindler_battery import SpindlerBattery
+from engines.capulet_engine import CapuletEngine
+from engines.sternman_engine import SternmanEngine
+from engines.willoughby_engine import WilloughbyEngine
 
-class CarFactory():
+
+class CarFactory:
+
+    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = CapuletEngine(current_mileage,last_service_mileage)
+        battery = SpindlerBattery(last_service_date,current_date)
+        car = Car(CapuletEngine,SpindlerBattery)
+        return car
+
+    def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = WilloughbyEngine(current_mileage,last_service_mileage)
+        battery = SpindlerBattery(last_service_date,current_date)
+        car = Car(CapuletEngine,SpindlerBattery)
+        return car
+    
+    def create_palindrome(current_date, last_service_date, warning_light_on):
+        engine = SternmanEngine(warning_light_on)
+        battery = SpindlerBattery(last_service_date,current_date)
+        car = Car(CapuletEngine,SpindlerBattery)
+        return car
+    
+    def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = WilloughbyEngine(current_mileage,last_service_mileage)
+        battery = NubbinBattery(last_service_date,current_date)
+        car = Car(CapuletEngine,SpindlerBattery)
+        return car
+    
+    def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = CapuletEngine(current_mileage,last_service_mileage)
+        battery = NubbinBattery(last_service_date,current_date)
+        car = Car(CapuletEngine,SpindlerBattery)
+        return car
+    
