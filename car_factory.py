17ecca802df2efa9d72935d028f39933d2d3093f
Nicholas Nasour <nicknasour@gmail.com>
2023-09-30 22:17:20 -0700

    finished test cases

--- car_factory.py
+++ car_factory.py
@@ -17,24 +17,24 @@ class CarFactory:
     def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
         engine = WilloughbyEngine(current_mileage,last_service_mileage)
         battery = SpindlerBattery(last_service_date,current_date)
-        car = Car(CapuletEngine,SpindlerBattery)
+        car = Car(WilloughbyEngine,SpindlerBattery)
         return car
     
     def create_palindrome(current_date, last_service_date, warning_light_on):
         engine = SternmanEngine(warning_light_on)
         battery = SpindlerBattery(last_service_date,current_date)
-        car = Car(CapuletEngine,SpindlerBattery)
+        car = Car(SternmanEngine,SpindlerBattery)
         return car
     
     def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
         engine = WilloughbyEngine(current_mileage,last_service_mileage)
         battery = NubbinBattery(last_service_date,current_date)
-        car = Car(CapuletEngine,SpindlerBattery)
+        car = Car(WilloughbyEngine,NubbinBattery)
         return car
     
     def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage):
         engine = CapuletEngine(current_mileage,last_service_mileage)
         battery = NubbinBattery(last_service_date,current_date)
-        car = Car(CapuletEngine,SpindlerBattery)
+        car = Car(CapuletEngine,NubbinBattery)
         return car
     
